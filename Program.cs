ecd50abdb0de2632e56628b89b78b2cd95e5dbbd
ADSKI-TaPOK748 <XE@XE4943>
2025-02-02 15:40:19 +0300

    что то

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.Design;
-
-void Nazvanie()
+void Nazvaniya()
 {
     string variable_string = "Это строка";
     int variable_int = 12;
@@ -39,7 +38,7 @@ void Nazvanie()
     xDouble = Int32.Parse(Console.ReadLine());
     Console.WriteLine(xDouble + " Ваше значение. ");
 }
-while(true)
+void Condition()
 {
     Console.WriteLine("Введите баланс");
 
@@ -63,3 +62,45 @@ else
     }
 }
 
+int i = 0;
+// цикл с пред усл
+while(i < 5)
+{
+    Console.WriteLine(i);
+    i++;
+}
+i = 0;
+// цикл с пост усл
+do
+{
+    i++;
+    Console.WriteLine(i);
+} while (i<0);
+// цикл фор
+for(i = 0;i<=52;i++)
+{
+    Console.WriteLine(i);
+}
+int x, y=1;
+for(i=2; i<=100;y++)
+{
+    x = i * y;
+    Console.WriteLine(x+"*"+i+"="+i*x);
+}
+int[] numbers;
+int[] numbers_1 = new int[10];
+int[] numbers_2 = new int[5] { 1, 5, 23, -4, 10 };
+int[] numbers_3 = { 1, 24, 3, -8 };
+int[,] matrix = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+
+foreach (int a in numbers_2);
+{
+    Console.WriteLine(a );
+}
+for(int b=0; b<numbers_2.Length;b++)
+{
+    Console.WriteLine(b);
+    Console.WriteLine(numbers_2[b]);
+    Console.WriteLine("в ячейке" + b + "значение" + numbers_2[b]);
+}
